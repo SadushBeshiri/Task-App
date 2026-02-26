5781f2821e71373ec3a062f02fb126b584381356
Sadush Beshiri <sadushbeshiri1@gmail.com>
2026-02-26 15:09:13 +0100

    Add cross origin for the frontend

--- task-backend/src/main/java/com/SadushBeshiri/task_backend/controller/TaskController.java
+++ task-backend/src/main/java/com/SadushBeshiri/task_backend/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.SadushBeshiri.task_backend.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import java.util.UUID;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(path = "/api/v1/tasks")
 public class TaskController {
