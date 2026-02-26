3b131ff9bad641f7c913a2e8929dc05a9c2104bb
Sadush Beshiri <sadushbeshiri1@gmail.com>
2026-02-26 13:29:19 +0100

    Create the mapper folder

---
+++ task-backend/src/main/java/com/SadushBeshiri/task_backend/domain/exception/mapper/TaskMapper.java
@@ -0,0 +1,18 @@
+package com.SadushBeshiri.task_backend.domain.exception.mapper;
+
+import com.SadushBeshiri.task_backend.domain.CreateTaskRequest;
+import com.SadushBeshiri.task_backend.domain.UpdateTaskRequest;
+import com.SadushBeshiri.task_backend.domain.dto.CreateTaskRequestDto;
+import com.SadushBeshiri.task_backend.domain.dto.TaskDto;
+import com.SadushBeshiri.task_backend.domain.dto.UpdateTaskRequestDto;
+import com.SadushBeshiri.task_backend.domain.entity.Task;
+
+public interface TaskMapper {
+
+    CreateTaskRequest fromDto(CreateTaskRequestDto dto);
+    
+    UpdateTaskRequest fromDto(UpdateTaskRequestDto dto);
+
+    TaskDto toDto(Task task);
+  
+}
---
+++ task-backend/src/main/java/com/SadushBeshiri/task_backend/domain/exception/mapper/impl/TaskMapperImpl.java
@@ -0,0 +1,46 @@
+package com.SadushBeshiri.task_backend.domain.exception.mapper.impl;
+
+import com.SadushBeshiri.task_backend.domain.CreateTaskRequest;
+import com.SadushBeshiri.task_backend.domain.UpdateTaskRequest;
+import com.SadushBeshiri.task_backend.domain.dto.CreateTaskRequestDto;
+import com.SadushBeshiri.task_backend.domain.dto.TaskDto;
+import com.SadushBeshiri.task_backend.domain.dto.UpdateTaskRequestDto;
+import com.SadushBeshiri.task_backend.domain.entity.Task;
+import com.SadushBeshiri.task_backend.domain.exception.mapper.TaskMapper;
+
+public class TaskMapperImpl implements TaskMapper{
+
+  @Override
+  public CreateTaskRequest fromDto(CreateTaskRequestDto dto) {
+    return new CreateTaskRequest(
+                dto.title(),
+                dto.description(),
+                dto.dueDate(),
+                dto.priority()
+        );
+  }
+
+  @Override
+  public UpdateTaskRequest fromDto(UpdateTaskRequestDto dto) {
+    return new UpdateTaskRequest(
+                dto.title(),
+                dto.description(),
+                dto.dueDate(),
+                dto.status(),
+                dto.priority()
+        );
+  }
+
+  @Override
+  public TaskDto toDto(Task task) {
+    return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                task.getPriority(),
+                task.getStatus()
+        );
+  }
+  
+}
